6d954c0d7db64d83c4ec5123dbdfe34e72ad2f1e
thearnavgarg <thearnavgarg@gmail.com>
2016-03-24 13:21:21 -0500

    Working of Firebase Highscore

--- app/src/main/java/me/arnavgarg/nimgame/MainActivity.java
+++ app/src/main/java/me/arnavgarg/nimgame/MainActivity.java
@@ -8,8 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.client.Firebase;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Firebase ref = new Firebase("https://nimgame.firebaseio.com/");
+    Button setUser;
+    Button setHighScore;
+
+    TextView user;
+    TextView highscore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        user = (TextView) findViewById(R.id.textViewUser);
+        highscore = (TextView) findViewById(R.id.textViewScore);
+        setUser = (Button) findViewById(R.id.buttonUser);
+        setHighScore = (Button) findViewById(R.id.buttonHighScore);
+
+        setUser.setOnClickListener(this);
+        setHighScore.setOnClickListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +65,28 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()) {
+
+            case R.id.buttonUser:
+                userMethod();
+                break;
+            case R.id.buttonHighScore:
+                userHighScore();
+                break;
+        }
+    }
+
+    public void userMethod() {
+        //TODO
+    }
+
+    public void userHighScore() {
+        //TODO
     }
 }
